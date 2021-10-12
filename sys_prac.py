2e33660bb17c579836ef58e8bf4ba13afdc0fe6c
Robin_Chen <sudacgb@gmail.com>
2021-10-11 22:48:13 -0400

    python module sys

---
+++ sys_prac.py
@@ -0,0 +1,7 @@
+# this file is used to practice system module
+import sys
+
+# you need to remember that the name of script is sys.argv[0]
+print('This is the name of the script ', sys.argv[0])
+print('Number of the arguments', len(sys.argv))
+print('The arguments are ', str(sys.argv))
